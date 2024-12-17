4b35b6846710024276e1fee3c56b9da14febabcd
Snowflyt <gaoge011022@163.com>
2024-12-17 16:44:21 +0800

    ✨ feat: Add solutions for question 17

--- src/17.ts
+++ src/17.ts
@@ -1,8 +1,13 @@
-export const compose = (f, g, h) => (a) => h(g(f(a)));
+export const compose =
+  <T, U, V, W>(f: (x: T) => U, g: (y: U) => V, h: (z: V) => W) =>
+  (a: T) =>
+    h(g(f(a)));
 
-export const upperCase = (x) => x.toUpperCase();
-export const lowerCase = (x) => x.toLowerCase();
-export const firstChar = (x) => x[0];
-export const firstItem = (x) => x[0];
-export const makeTuple = (x) => [x];
-export const makeBox = (value) => ({ value });
+type FirstChar<S extends string> = S extends `${infer C}${string}` ? C : never;
+
+export const upperCase = <S extends string>(x: S): Uppercase<S> => x.toUpperCase() as Uppercase<S>;
+export const lowerCase = <S extends string>(x: S): Lowercase<S> => x.toLowerCase() as Lowercase<S>;
+export const firstChar = <S extends string>(x: S): FirstChar<S> => x[0] as FirstChar<S>;
+export const firstItem = <Head>(x: readonly [Head, ...unknown[]]): Head => x[0];
+export const makeTuple = <T>(x: T): [T] => [x];
+export const makeBox = <T>(value: T): { value: T } => ({ value });
