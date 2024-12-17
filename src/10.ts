fbd3e91ad1852ba0bc3d96400297f7369b56c607
Snowflyt <gaoge011022@163.com>
2024-12-17 15:47:40 +0800

    ✨ feat: Add solutions for question 10

--- src/10.ts
+++ src/10.ts
@@ -1,1 +1,11 @@
-export enum Gift {}
+export enum Gift {
+  Coal = 0,
+  Train = 1 << 0,
+  Bicycle = 1 << 1,
+  SuccessorToTheNintendoSwitch = 1 << 2,
+  TikTokPremium = 1 << 3,
+  Vape = 1 << 4,
+  Traditional = Train | Bicycle,
+  OnTheMove = Coal | Bicycle | TikTokPremium | Vape,
+  OnTheCouch = (OnTheMove & ~Bicycle) | SuccessorToTheNintendoSwitch,
+}
