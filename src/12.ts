5427b9401bf45587f8240d885d07ff8538759d3b
Snowflyt <gaoge011022@163.com>
2024-12-17 16:02:12 +0800

    ✨ feat: Add solutions for question 12

--- src/12.ts
+++ src/12.ts
@@ -1,2 +1,12 @@
-type NaughtyOrNice = unknown;
-export type FormatNames = unknown;
+type Gender = "M" | "F";
+
+type NaughtyOrNice<Item extends readonly [string, Gender, `${number}`]> =
+  Item[0] extends "Yanni" | "Petra" | "Aagya" ? "nice" : "naughty";
+
+export type FormatNames<Names extends readonly (readonly [string, Gender, `${number}`])[]> = {
+  [I in keyof Names]: {
+    name: Names[I][0];
+    count: Names[I][2] extends `${infer N extends number}` ? N : never;
+    rating: NaughtyOrNice<Names[I]>;
+  };
+};
