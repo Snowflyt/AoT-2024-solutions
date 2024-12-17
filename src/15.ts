ff7dc8563e25e8843d5ccb5c08a87636a2d40b0c
Snowflyt <gaoge011022@163.com>
2024-12-17 16:28:39 +0800

    ✨ feat: Add solutions for question 15

--- src/15.ts
+++ src/15.ts
@@ -1,1 +1,23 @@
-export type GetRoute<T extends string> = unknown;
+type TrimDash<S extends string> =
+  S extends `-${infer Rest}` ? TrimDash<Rest>
+  : S extends `${infer Rest}-` ? TrimDash<Rest>
+  : S;
+
+// `_GetRoute` assumes:
+//   - No padding or trailing dashes
+//   - `T` is not empty string
+type _GetRoute<
+  S extends string,
+  CurrDistance extends void[] = [],
+  CurrName extends string = "",
+  Result extends [string, number][] = [],
+> =
+  S extends `${infer C}${infer Rest}` ?
+    C extends "-" ?
+      CurrName extends "" ?
+        _GetRoute<Rest, [...CurrDistance, void], "", Result>
+      : _GetRoute<Rest, [void], "", [...Result, [CurrName, CurrDistance["length"]]]>
+    : _GetRoute<Rest, CurrDistance, `${CurrName}${C}`, Result>
+  : [...Result, [CurrName, CurrDistance["length"]]];
+
+export type GetRoute<S extends string> = TrimDash<S> extends "" ? [] : _GetRoute<TrimDash<S>>;
