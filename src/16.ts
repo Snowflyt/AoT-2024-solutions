8ac9f2d1b2003bdf1d99edef5f266cf2f098cd8b
Snowflyt <gaoge011022@163.com>
2024-12-17 16:36:50 +0800

    ✨ feat: Add solutions for question 16

--- src/16.ts
+++ src/16.ts
@@ -1,1 +1,8 @@
-export declare function DynamicParamsCurrying(): unknown;
+export declare function DynamicParamsCurrying<Params extends unknown[], R>(
+  f: (...args: Params) => R,
+): Curry<Params, R>;
+
+type Curry<Unscanned extends unknown[], R, Scanned extends unknown[] = [], Result = unknown> =
+  Unscanned extends [infer Head, ...infer Tail] ?
+    Curry<Tail, R, [...Scanned, Head], Result & ((...args: Scanned) => Curry<Unscanned, R>)>
+  : Result & ((...args: Scanned) => R);
